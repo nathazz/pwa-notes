74d46ec1fae1d34e524d15e56e72c3bec35b1a03
nathazz <nathansg907@gmail.com>
2025-07-30 17:45:52 -0300

    refactor: enhance service worker caching and add offline page

--- js/sw.js
+++ js/sw.js
@@ -1,19 +1,44 @@
 importScripts(
-  "https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js",
+  "https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js"
 );
+
+
+workbox.precaching.precacheAndRoute([
+  { url: "/offline.html", revision: null },
+]);
 
 workbox.routing.registerRoute(
   ({ request }) => request.destination === "image",
-  new workbox.strategies.CacheFirst(),
+  new workbox.strategies.CacheFirst({
+    cacheName: "images-cache",
+    plugins: [
+      new workbox.expiration.ExpirationPlugin({
+        maxEntries: 50,
+        maxAgeSeconds: 30 * 24 * 60 * 60, 
+      }),
+    ],
+  })
 );
 
 workbox.routing.registerRoute(
   ({ request }) =>
     request.destination === "script" || request.destination === "style",
-  new workbox.strategies.StaleWhileRevalidate(),
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: "static-resources",
+  })
 );
 
 workbox.routing.registerRoute(
-  ({ url }) => url.pathname === "/",
-  new workbox.strategies.NetworkFirst(),
+  ({ url }) => url.pathname === "/" || url.pathname === "/index.html",
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: "html-cache",
+  })
 );
+
+workbox.routing.setCatchHandler(async ({ event }) => {
+  if (event.request.destination === "document") {
+    return caches.match("/offline.html");
+  }
+
+  return Response.error();
+});
